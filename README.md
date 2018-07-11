1748cb9ac4442c7e6a84c9eda8cf89e77d3ab980
Kawamura Shintaro <kawasin73@gmail.com>
2018-07-11 23:09:57 +0900

    Create wanpoll

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # wanpoll
 
-`WAN IP` をポーリングして監視し、変更を検知したら `Route53` の Aレコード を書き換えるコマンドです。
+`WAN IP` をポーリングして監視し、変更を検知したら `Route53` の `Aレコード` を書き換えるコマンドです。
+
+一定間隔でIPアドレスを確認し、変更されている時のみ Route53 にAPI経由でレコードを書き換えます。
+
+ルーターに一時的に接続できなくなった場合や Route53 のリクエストに失敗した場合は、標準エラー出力にエラーを出力しポーリングを継続します。
+
+自宅のルーターが DDNS (Dynamic DNS) に対応していなかったため、ルーターの設定ページをハックして IP をぶっ抜いて動的なIPアドレスの変更を DNS に通知します。
 
 ## 対象
 
@@ -8,4 +14,101 @@
 
 `http://172.16.255.254/GetWanIP.html` にアクセスして WanIP をぶっこぬきます。
 
+## Install
+
+パッケージマネージャーに `dep` を利用しています。
+
+```
+$ git clone https://github.com/kawasin73/wanpoll.git && cd wanpoll
+$ dep ensure
+$ go build
+```
+
+## Example
+
+```
+$ ./wanpoll -hz xxxxxxxxxx -name=home.kawasin73.com,usa-reminder.kawasin73.com -user=xxxxxxxx -password=xxxxxxx -interval=10 -ttl=10
+2018/07/11 23:00:23 detect new ip address "" -> "60.xxx.xxx.xxx"
+2018/07/11 23:00:24 update record "home.kawasin73.com" == "60.xxx.xxx.xxx"
+2018/07/11 23:00:24 update record "usa-reminder.kawasin73.com" == "60.xxx.xxx.xxx"
+```
+
 ## オプション
+
+```
+$ go build && ./wanpoll -h
+Usage of ./wanpoll:
+  -hz string
+    	hosted zone Id in route53
+  -interval int
+    	interval time polling to routerIpPage (second) (default 1)
+  -name string
+    	recode name separated with ',' comma
+  -password string
+    	basic auth password for routerIpPage
+  -region string
+    	aws region where route53 is placed (default "ap-northeast-1")
+  -ttl int
+    	dns record ttl (second) (default 60)
+  -url string
+    	routerIpPage url which shows global wan ip address (default "http://172.16.255.254/GetWanIP.html")
+  -user string
+    	basic auth username for routerIpPage
+```
+
+## AWS の Route53
+
+`wanpoll` は AWS の Route53 にのみ対応しています。
+Route53 の Hosted Zone のID を調べておく必要があります。
+
+レコードの更新は、`ChangeResourceRecordSets` API を利用します。
+
+- https://docs.aws.amazon.com/Route53/latest/APIReference/API_ChangeResourceRecordSets.html
+
+### 認証
+
+Route53 のリージョンは、オプションで設定します。
+
+Route53 にアクセスする認証情報は、以下の3つの方法で設定できます。
+
+- EC2 の IAM role (自宅のソフトバンク光の環境から使うことを想定しているので、この方法は利用しないはずです)
+- shared credential file
+- 環境変数
+  - `AWS_ACCESS_KEY_ID`
+  - `AWS_SECRET_ACCESS_KEY`
+  - `AWS_SESSION_TOKEN` (optional) (一時トークンは使うのは難しいので使わないと思います)
+
+詳しくは以下の URL を参照してください。
+
+- https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials
+
+### 権限
+
+IAM には、インラインポリシーとして、以下のポリシーを追加します。
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "<Sid>",
+            "Effect": "Allow",
+            "Action": [
+                "route53:ChangeResourceRecordSets"
+            ],
+            "Resource": [
+                "arn:aws:route53:::hostedzone/<zoneid>"
+            ]
+        }
+    ]
+}
+```
+
+参考URLは以下の通りです。
+
+- https://docs.aws.amazon.com/ja_jp/Route53/latest/DeveloperGuide/r53-api-permissions-ref.html#required-permissions-resource-record-sets
+- https://docs.aws.amazon.com/ja_jp/general/latest/gr/aws-arns-and-namespaces.html#arn-syntax-route53
+
+## LICENSE
+
+MIT
--- main.go
+++ main.go
@@ -1,5 +1,155 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/pkg/errors"
+)
+
+var (
+	routerUrl    = flag.String("url", "http://172.16.255.254/GetWanIP.html", "routerIpPage url which shows global wan ip address")
+	user         = flag.String("user", "", "basic auth username for routerIpPage")
+	password     = flag.String("password", "", "basic auth password for routerIpPage")
+	interval     = flag.Int("interval", 1, "interval time polling to routerIpPage (second)")
+	region       = flag.String("region", "ap-northeast-1", "aws region where route53 is placed")
+	hostedZoneId = flag.String("hz", "", "hosted zone Id in route53")
+	recordNames  = flag.String("name", "", "recode name separated with ',' comma")
+	ttl          = flag.Int64("ttl", 60, "dns record ttl (second)")
+)
+
+func main() {
+	flag.Parse()
+	if *interval < 1 {
+		*interval = 1
+	}
+	if *hostedZoneId == "" {
+		log.Println("hz (hosted zone id) is required")
+		os.Exit(1)
+		return
+	}
+	records := strings.Split(*recordNames, ",")
+	if len(records) == 0 {
+		log.Println("record (record name) is required")
+		os.Exit(1)
+		return
+	}
+	if *ttl < 1 {
+		log.Println("ttl must be greater than 0")
+		os.Exit(1)
+		return
+	}
+
+	duration := time.Duration(*interval) * time.Second
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(*region),
+	})
+	if err != nil {
+		log.Println("failed to create aws session :", err)
+		os.Exit(1)
+		return
+	}
+	rt := route53.New(sess)
+
+	var currentIp string
+
+	for {
+		ip, err := fetchIp(*routerUrl, *user, *password)
+		if err != nil {
+			log.Println("failed to fetch ip address :", err)
+			time.Sleep(duration)
+			continue
+		}
+		if ip == currentIp {
+			time.Sleep(duration)
+			continue
+		}
+
+		log.Printf("detect new ip address \"%v\" -> \"%v\"\n", currentIp, ip)
+
+		err = setDNS(rt, *hostedZoneId, records, *ttl, ip)
+		if err != nil {
+			log.Println("failed to set dns :", err)
+			time.Sleep(duration)
+			continue
+		}
+
+		for _, r := range records {
+			log.Printf("update record \"%v\" == \"%v\"\n", r, ip)
+		}
+
+		currentIp = ip
+		time.Sleep(duration)
+	}
+}
+
+var (
+	ipMatcher     = regexp.MustCompile(`([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})`)
+	errIpNotFound = errors.New("ip address not found in response")
+)
+
+func fetchIp(url, user, password string) (string, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", errors.Wrap(err, "build request")
+	}
+	req.SetBasicAuth(user, password)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "send get request")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return "", errors.New(resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "read body")
+	}
+	ip := ipMatcher.Find(body)
+	if ip == nil {
+		return "", errIpNotFound
+	}
+	return string(ip), nil
+}
+
+func setDNS(rt *route53.Route53, zoneId string, records []string, ttl int64, ip string) error {
+	params := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: make([]*route53.Change, len(records)),
+			Comment: aws.String(fmt.Sprintf("changed at %v", time.Now())),
+		},
+		HostedZoneId: aws.String(zoneId),
+	}
+	for i, record := range records {
+		params.ChangeBatch.Changes[i] = &route53.Change{
+			Action: aws.String(route53.ChangeActionUpsert),
+			ResourceRecordSet: &route53.ResourceRecordSet{
+				Name: aws.String(record),
+				ResourceRecords: []*route53.ResourceRecord{
+					{
+						Value: aws.String(ip),
+					},
+				},
+				TTL:  aws.Int64(ttl),
+				Type: aws.String(route53.RRTypeA),
+			},
+		}
+	}
+	_, err := rt.ChangeResourceRecordSets(params)
+	if err != nil {
+		return err
+	}
+	return nil
 }
